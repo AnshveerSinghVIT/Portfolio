ee387387812675bd0e246392f3ff56aa5de7f29b
Anshveer Singh <singhanshveer73@gmail.com>
2026-07-17 19:30:50 +0530

    Remove email contact from maintenance page

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,6 @@ export default function Home() {
     <main style={{ padding: '50px', fontFamily: 'sans-serif' }}>
       <h1>Anshveer Singh</h1>
       <p>Site is under maintenance.</p>
-      <p>Contact: <a href="mailto:singhanshveer73@gmail.com">singhanshveer73@gmail.com</a></p>
     </main>
   );
 }
